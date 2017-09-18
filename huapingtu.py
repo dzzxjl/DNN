5ac7ebdddf4c63fa1dd66d2658d0f855f72d9c9a
dzzxjl <dzzxjl@126.com>
2017-09-18 21:59:12 +0800

    update

---
+++ huapingtu.py
@@ -0,0 +1,18 @@
+import matplotlib.pylab as plt
+
+def huabingtu(map):
+    labels = []
+    sizes = []
+    for x in map:
+        labels.append(x)
+        sizes.append(map[x])
+    # labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    # sizes = [15, 30, 45, 10]
+    # explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.show()
